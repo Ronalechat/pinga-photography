32c5467b72237f029c1b39759a14883fe88ccbf8
ronalechat <dapop@hotmail.com>
2026-04-17 20:39:30 +1000

    Deployment fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import { getStoryblokApi, getVersion } from '@/utils/storyblok'
 import { StoryblokServerStory, StoryblokLiveEditing } from '@storyblok/react/rsc'
+import { notFound } from 'next/navigation'
 
 export const revalidate = 60
 
 export default async function Home() {
   const version = await getVersion()
   const sb = getStoryblokApi()
-  const { data } = await sb.get('cdn/stories/home', { version })
+
+  let data
+  try {
+    const res = await sb.get('cdn/stories/home', { version })
+    data = res.data
+  } catch (err: unknown) {
+    if ((err as { status?: number }).status === 404) notFound()
+    throw err
+  }
 
   return (
     <>
